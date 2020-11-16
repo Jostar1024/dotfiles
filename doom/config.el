56ce571dc6f0fa01124bb59f8b8b974fabcc251a
Yucheng CAO <howard.eureka@gmail.com>
2020-11-16 11:13:10 +0800

    add paste code from external

--- doom/config.el
+++ doom/config.el
@@ -112,6 +112,34 @@
      (:foreground "grey40")))
     "Elixir dim face.")
 
+;; paste code from external
+;; https://stackoverflow.com/questions/18691973/is-there-a-set-paste-option-in-emacs-to-paste-paste-from-external-clipboard?spm=a2c4e.10696291.0.0.111219a4sTbBnB
+(defvar ttypaste-mode nil)
+(add-to-list 'minor-mode-alist '(ttypaste-mode " Paste"))
+
+(defun ttypaste-mode ()
+  (interactive)
+  (let ((buf (current-buffer))
+        (ttypaste-mode t))
+    (with-temp-buffer
+      (let ((stay t)
+            (text (current-buffer)))
+        (redisplay)
+        (while stay
+          (let ((char (let ((inhibit-redisplay t)) (read-event nil t 0.1))))
+            (unless char
+              (with-current-buffer buf (insert-buffer-substring text))
+              (erase-buffer)
+              (redisplay)
+              (setq char (read-event nil t)))
+            (cond
+             ((not (characterp char)) (setq stay nil))
+             ((eq char ?\r) (insert ?\n))
+             ((eq char ?\e)
+              (if (sit-for 0.1 'nodisp) (setq stay nil) (insert ?\e)))
+             (t (insert char)))))
+                (insert-buffer-substring text)))))
+
 (use-package! elixir-mode
   :mode
   (("\\.eex\\'" . web-mode)
