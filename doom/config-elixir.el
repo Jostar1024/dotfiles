5c33e4da2e734bf2d40125315034bee7c16df265
Yucheng CAO <howard.eureka@gmail.com>
2020-12-01 16:06:54 +0800

    def will not complete on do..end

--- doom/config-elixir.el
+++ doom/config-elixir.el
@@ -8,6 +8,10 @@
     "Elixir dim face.")
 
 (use-package! elixir-mode
+  :init
+  ;; Disable default smartparens config. There are too many pairs; we only want
+  ;; a subset of them (defined below).
+  (provide 'smartparens-elixir)
   :mode
   (("\\.eex\\'" . web-mode)
    ("\\.leex\\'" . web-mode))
@@ -24,6 +28,15 @@
                             ("\\_<false\\_>" . font-lock-constant-face)
                             ("\\_<nil\\_>" . font-lock-constant-face)
                             ("\\_<_[a-zA-Z0-9]*\\_>" . '+elixir-dim-face)))
+
+  ;; ...and only complete the basics
+  (sp-with-modes 'elixir-mode
+    (sp-local-pair "do" "end"
+                   :when '(("RET" "<evil-ret>"))
+                   :unless '(sp-in-comment-p sp-in-string-p)
+                   :post-handlers '("||\n[i]"))
+    (sp-local-pair "do " " end" :unless '(sp-in-comment-p sp-in-string-p))
+    (sp-local-pair "fn " " end" :unless '(sp-in-comment-p sp-in-string-p)))
 
   (modify-syntax-entry ?& "'" elixir-mode-syntax-table))
 
