311252f9bfb2d485a5b847d4a61450b1583728b0
Yucheng CAO <howard.eureka@gmail.com>
2021-01-04 14:43:05 +0800

    use smaller font; add org-agenda files

--- doom/config.el
+++ doom/config.el
@@ -29,12 +29,13 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
-(setq doom-font (font-spec :family "Fira Code" :size 14)
-      doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Fira Code" :size 10)
+      doom-variable-pitch-font (font-spec :family "sans" :size 9))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/org")
+(setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
