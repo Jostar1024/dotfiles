ddbe0e874717735cfa51b1b4fee2b23085a3da91
Yucheng CAO <howard.eureka@gmail.com>
2021-08-27 10:38:39 -0400

    update doom config

--- doom/.doom.d/config-org.el
+++ doom/.doom.d/config-org.el
@@ -114,11 +114,11 @@
       (when in-latex
         (setq +org-last-in-latex t)))))
 
-(define-minor-mode org-latex-auto-toggle
-  "Auto toggle latex overlay when cursor enter/leave."
-  nil
-  nil
-  nil
-  (if org-latex-auto-toggle
-      (add-hook 'post-command-hook '+org-post-command-hook nil t)
-    (remove-hook 'post-command-hook '+org-post-command-hook t)))
+;; (define-minor-mode org-latex-auto-toggle
+;;   "Auto toggle latex overlay when cursor enter/leave."
+;;   nil
+;;   nil
+;;   nil
+;;   (if org-latex-auto-toggle
+;;       (add-hook 'post-command-hook '+org-post-command-hook nil t)
+;;     (remove-hook 'post-command-hook '+org-post-command-hook t)))
