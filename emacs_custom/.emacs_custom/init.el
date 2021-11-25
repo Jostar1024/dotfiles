8aad525fbf6637cac04ceaf0df3f60777010bf5d
Yucheng Cao <yuchengcao@tubi.tv>
2021-11-25 14:07:47 +0800

    add new emacs config

---
+++ emacs_custom/.emacs_custom/init.el
@@ -0,0 +1,27 @@
+;;; -*- lexical-binding: t; -*-
+
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install use-package
+(straight-use-package 'use-package)
+
+;; Configure use-package to use straight.el by default
+(use-package straight
+             :custom (straight-use-package-by-default t))
+
+(use-package evil
+             :init (setq evil-want-C-i-jump nil)
+             :config (evil-mode))
+
