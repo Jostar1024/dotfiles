b537da958e071f14a4981634f041e7474d8bb63d
Yucheng <howard.eureka@gmail.com>
2021-11-30 09:35:56 +0800

    update projectile

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -165,3 +165,10 @@
   :custom
   (dired-dwim-target t))
 
+(use-package! projectile
+  :init
+  (setq projectile-project-root-files #'( ".projectile" ))
+  (setq projectile-project-root-files-functions #'(projectile-root-top-down
+                                             projectile-root-top-down-recurring
+                                             projectile-root-bottom-up
+                                             projectile-root-local)))
