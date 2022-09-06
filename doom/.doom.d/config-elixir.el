5da0d51e9a896a05ec8d116f0c23930125e2461c
Yucheng Cao <howard.eureka@gmail.com>
2022-09-06 10:43:03 +0800

    update elixir config

--- doom/.doom.d/config-elixir.el
+++ doom/.doom.d/config-elixir.el
@@ -12,8 +12,6 @@
   ;; Disable default smartparens config. There are too many pairs; we only want
   ;; a subset of them (defined below).
   (provide 'smartparens-elixir)
-  :hook
-  (elixir-mode . paredit-mode)
   :config
   (font-lock-add-keywords 'elixir-mode
                           '(("\\([_a-zA-Z0-9!?]+\\):" 1 'default)
@@ -38,7 +36,6 @@
         :map elixir-mode-map
         :localleader
         :n "f" #'elixir-format
-        :n "t" #'+elixir-test-current-file
         :n "d" #'lsp-ui-doc-glance
         :n "c c" #'inf-iex-eval
         :n "c v" #'inf-iex-toggle-send-target)
@@ -50,68 +47,9 @@
   :init
   (evil-set-initial-state 'inf-iex-tracer-mode 'motion))
 
-;; (defun +toggle-ex-leex ()
-;;   (interactive)
-;;   (cond
-;;    ((string-suffix-p ".ex" (buffer-file-name))
-;;     (find-file (string-replace ".ex" ".html.leex" (buffer-file-name))))
-;;    ((string-suffix-p ".html.leex" (buffer-file-name))
-;;     (let ((sym (thing-at-point 'symbol)))
-;;       (find-file (string-replace ".html.leex" ".ex" (buffer-file-name)))
-;;       (when sym
-;;         (let (pos)
-;;           (save-mark-and-excursion
-;;             (goto-char (point-min))
-;;             (setq pos (re-search-forward (format "\"%s\"" (regexp-quote sym)) nil t)))
-;;           (when pos (goto-char pos) (recenter))))))
-;;    (t
-;;     (error "File extension is neither .ex nor .html.leex"))))
-
-;; (define-key elixir-mode-map (kbd "C-c C-q") '+toggle-ex-leex)
-;; (define-key web-mode-map (kbd "C-c C-q") '+toggle-ex-leex)
-
-;; (defun +toggle-ex-leex ()
-;;   (interactive)
-;;   (cond
-;;    ((string-suffix-p ".ex" (buffer-file-name))
-;;     (find-file (string-replace ".ex" ".html.leex" (buffer-file-name))))
-;;    ((string-suffix-p ".html.leex" (buffer-file-name))
-;;     (let ((sym (thing-at-point 'symbol)))
-;;       (find-file (string-replace ".html.leex" ".ex" (buffer-file-name)))
-;;       (when sym
-;;         (re-search-forward (regexp-quote sym) nil t))
-;;       (recenter)))
-;;    (t
-;;     (error "File extension is neither .ex nor .html.leex"))))
-
-(require 'dash)
-
-(defun +elixir-test-current-file ()
-  (interactive)
-  (let* ((str-list             (split-string (projectile-project-root) "\/"))
-         (trimed-list          (-remove (lambda (x) (string= x "")) str-list))
-         (dropped              (length trimed-list))
-         (filename-list        (split-string (buffer-file-name) "\/"))
-         (filename-trimed-list (-remove (lambda (x) (string= x "")) filename-list))
-         (filename             (mapconcat 'identity (-drop dropped filename-trimed-list) "\/")))
-    (inf-iex--tmux-send (concat "mix test " filename))))
-
 (defun +copy-file-name ()
   (interactive)
-  (kill-new (buffer-file-name))
-  )
-;; elixir should have a cider-doc
-;; (defun cider-doc (&optional arg)
-;;   "Open Clojure documentation in a popup buffer.
-
-;; Prompts for the symbol to use, or uses the symbol at point, depending on
-;; the value of `cider-prompt-for-symbol'.  With prefix arg ARG, does the
-;; opposite of what that option dictates."
-;;   (interactive "P")
-;;   (cider-ensure-connected)
-;;   (funcall (cider-prompt-for-symbol-function arg)
-;;            "Doc for"
-;;            #'cider-doc-lookup))
+  (kill-new (buffer-file-name)))
 
 (use-package! exunit
   :hook (elixir-mode . exunit-mode)
