fbb9a4e2dd1426d1515856ab2c5280a5bbd4a7e8
Yucheng Cao <yuchengcao@tubi.tv>
2024-01-22 10:42:08 +0800

    [doom] add languages

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -132,7 +132,7 @@
        (haskell +lsp)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
-       ;;json              ; At least it ain't XML
+       json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        julia             ; a better, faster MATLAB
@@ -144,7 +144,7 @@
        (lua +fennel)               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;; nim               ; python + lisp at the speed of c
-       ;; nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;; ocaml             ; an objective camel
        (org
         +journal)              ; organize your plain life in plain text
