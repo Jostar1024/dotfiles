08ad1ee0cc69423966a1e98d2759597aa7285008
Yucheng Cao <yuchengcao@tubi.tv>
2024-05-12 11:04:51 +0800

    [nix] update macos config

--- home-manager/home/common.nix
+++ home-manager/home/common.nix
@@ -4,9 +4,59 @@
   pkgs,
   ...
 }: {
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = (_: true);
+  };
   home.packages = with pkgs; [
+    stow
     curl
     wget
     neofetch
+
+    # programming
+    asdf-vm
+    elixir-ls
+    terraform
+    clj-kondo
+    cljfmt
+    stylelint
+    nodePackages.js-beautify
+    shellcheck
+    rustup
+    rustc
+    dockfmt
+    shfmt
+    nixfmt
+    racket
+    pandoc
+    haskellPackages.lsp
+    haskellPackages.cabal-install
+
+    # utils
+    ripgrep
+    findutils
+    fd
+    fzf
+    jq
+    yq-go
+    eza
+    comma
+    
+    # libs
+    librime
+    fontconfig
+    coreutils
+    aspell
+
+    # softwares    
+    alacritty
+    syncthing
+    keepassxc
+
+    # fonts
+    jetbrains-mono
+    lxgw-wenkai
   ];
+  programs.home-manager.enable = true;
 }
--- home-manager/home/darwin.nix
+++ home-manager/home/darwin.nix
@@ -9,5 +9,38 @@
   home.stateVersion = "23.11";
   home.packages = with pkgs; [
     emacs
+    iterm2
   ];
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    shellAliases = {
+      ls = "eza";
+      l = "eza -lh";
+      la = "eza -la";
+      ip = "ip --color=auto";
+      hms = "home-manager switch";
+    };
+    initExtra = "
+      export PATH=$PATH:~/.config/emacs/bin
+      source ~/.zshrc
+    ";
+    profileExtra = "
+      eval \"$(/opt/homebrew/bin/brew shellenv)\"
+    ";
+    dotDir = ".config/zsh";
+  };
 }
