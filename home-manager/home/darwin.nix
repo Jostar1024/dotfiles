f3e0210deb09ad05163cbd4754b66ea12133e2bb
Yucheng Cao <yuchengcao@tubi.tv>
2024-06-02 21:25:12 +0800

    [nix] remove redundancy

--- home-manager/home/darwin.nix
+++ home-manager/home/darwin.nix
@@ -39,39 +39,4 @@
     enable = true;
     enableZshIntegration = true;
   };
-
-  programs.zsh = {
-    enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    history.ignoreDups = true;
-    shellAliases = {
-      ls = "eza";
-      l = "eza -lh";
-      la = "eza -la";
-      hms = "home-manager switch";
-      tn = "tmux new-session -A -s";
-      ta = "tmux attach -t";
-      k = "kubectl";
-      cat = "bat --style=plain --pager=never";
-    };
-    initExtra = "
-      export LANG=en_US.UTF-8
-      export PATH=$PATH:~/.config/emacs/bin
-      export KERL_BUILD_DOCS=yes
-      export KERL_INSTALL_MANPAGES=yes
-      export KERL_INSTALL_HTMLDOCS=yes
-      export LESS=\"-SRXF\"
-
-      source ~/.zshrc
-
-      eval \"$(starship init zsh)\"
-      . \"$HOME/.nix-profile/share/asdf-vm/asdf.sh\"
-      . \"$HOME/.nix-profile/share/bash-completion/completions/asdf.bash\"
-    ";
-    profileExtra = "
-      eval \"$(/opt/homebrew/bin/brew shellenv)\"
-    ";
-    dotDir = ".config/zsh";
-  };
 }
