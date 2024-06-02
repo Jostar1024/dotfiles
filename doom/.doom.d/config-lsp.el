2434ba2df38de3dec6a64f46c85e44ec14b18277
Yucheng Cao <yuchengcao@tubi.tv>
2024-06-02 18:53:43 +0800

    [doom] use lsp-booster

--- doom/.doom.d/config-lsp.el
+++ doom/.doom.d/config-lsp.el
@@ -18,7 +18,33 @@
   (lsp-signature-render-documentation nil)
   ;; (lsp-completion-show-detail nil)
   ;; (lsp-completion-show-kind t)
-  (lsp-elixir-server-command '("/home/yucheng/community/elixir-ls/release/language_server.sh")))
+  (lsp-elixir-server-command '("elixir-ls"))
+  :config
+  (defun lsp-booster--advice-json-parse (old-fn &rest args)
+    "Try to parse bytecode instead of json."
+    (or
+     (when (equal (following-char) ?#)
+       (let ((bytecode (read (current-buffer))))
+         (when (byte-code-function-p bytecode)
+           (funcall bytecode))))
+     (apply old-fn args)))
+  (advice-add 'json-parse-buffer :around #'lsp-booster--advice-json-parse)
+
+  (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+      (if (and (not test?)                             ;; for check lsp-server-present?
+               (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+               lsp-use-plists
+               (not (functionp 'json-rpc-connection))  ;; native json-rpc
+               (executable-find "emacs-lsp-booster"))
+          (progn
+            (message "Using emacs-lsp-booster for %s!" orig-result)
+            (cons "emacs-lsp-booster" orig-result))
+        orig-result)))
+  (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
+  )
+
 
 (use-package! lsp-ui
   :hook
