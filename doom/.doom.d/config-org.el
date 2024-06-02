e41feac97613d8a2da5225770ff3ee5d703389ae
Yucheng Cao <yuchengcao@tubi.tv>
2024-06-02 18:54:15 +0800

    [doom] add org map

--- doom/.doom.d/config-org.el
+++ doom/.doom.d/config-org.el
@@ -1,5 +1,11 @@
 ;;; ../.dotfiles/doom/.doom.d/config-org.el -*- lexical-binding: t; -*-
 
+(use-package! org
+  :init
+  (map! :after org
+        :map org-mode-map
+        :n "TAB" #'org-cycle)
+  )
 ;; (use-package! org-roam
 ;;   :custom
 ;;   (org-format-latex-options (plist-put org-format-latex-options :scale 4.0)))
