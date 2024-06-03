3a3005430d52821c91e83204e17a31ec6e540a46
Yucheng Cao <yuchengcao@tubi.tv>
2024-06-03 13:59:46 +0800

    [nix] format

--- home-manager/programs/tmux.nix
+++ home-manager/programs/tmux.nix
@@ -1,14 +1,18 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   programs.tmux = {
     enable = true;
     keyMode = "vi";
     historyLimit = 30000;
     customPaneNavigationAndResize = true;
     terminal = "screen-256color";
-    plugins = with pkgs; [ tmuxPlugins.sensible
-                           tmuxPlugins.yank
-                         ];
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.yank
+    ];
   };
 }
