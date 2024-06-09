f4331935917be0b1f955eab19db3788f38243772
Yucheng Cao <yuchengcao@tubi.tv>
2024-06-09 22:25:23 +0800

    [nix] beautify tmux

--- home-manager/programs/tmux.nix
+++ home-manager/programs/tmux.nix
@@ -13,6 +13,35 @@
     plugins = with pkgs; [
       tmuxPlugins.sensible
       tmuxPlugins.yank
+      tmuxPlugins.catppuccin
     ];
+    prefix = "C-s";
+    mouse = true;
+    extraConfig = ''
+      unbind r
+      bind-key r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded."
+      set-option -g status-position top
+
+      # catppuccin config 3 from:
+      # https://github.com/catppuccin/tmux
+      set -g @catppuccin_window_left_separator ""
+      set -g @catppuccin_window_right_separator " "
+      set -g @catppuccin_window_middle_separator " █"
+      set -g @catppuccin_window_number_position "right"
+
+      set -g @catppuccin_window_default_fill "number"
+      set -g @catppuccin_window_default_text "#W"
+
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#W"
+
+      set -g @catppuccin_status_modules_right "session"
+      set -g @catppuccin_status_left_separator  " "
+      set -g @catppuccin_status_right_separator ""
+      set -g @catppuccin_status_fill "icon"
+      set -g @catppuccin_status_connect_separator "no"
+
+      set -g @catppuccin_directory_text "#{pane_current_path}"
+    '';
   };
 }
