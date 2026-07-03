5e6113b20d897a6551cf6bc48b3a82b7ce688374
Yucheng Cao <yuchengcao@tubitv.com>
2026-07-03 14:22:40 +0800

    [custom] move use-package emacs to init

--- emacs_custom/.emacs_custom/elisp/init-completion.el
+++ emacs_custom/.emacs_custom/elisp/init-completion.el
@@ -1,36 +1,7 @@
 ;; init-completion.el --- init of emacs -*- lexical-binding: t; -*-
 
 ;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; TAB cycle if there are only few candidates
-  (setq completion-cycle-threshold 3)
 
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (setq tab-always-indent 'complete)
-  :custom
-  (help-window-select t "Switch to help buffers automatically")
-  ;; https://github.com/minad/vertico#configuration
-  ;; Enable context menu. `vertico-multiform-mode' adds a menu in the minibuffer
-  ;; to switch display modes.
-  (context-menu-mode t)
-  ;; Support opening new minibuffers from inside existing minibuffers.
-  (enable-recursive-minibuffers t)
-  ;; Hide commands in M-x which do not work in the current mode.  Vertico
-  ;; commands are hidden in normal buffers. This setting is useful beyond
-  ;; Vertico.
-  (read-extended-command-predicate #'command-completion-default-include-p)
-  ;; Do not allow the cursor in the minibuffer prompt
-  (minibuffer-prompt-properties
-   '(read-only t cursor-intangible t face minibuffer-prompt))
-  :bind (("M-g TAB" . nil))
-  )
 
 (use-package orderless
   :custom
