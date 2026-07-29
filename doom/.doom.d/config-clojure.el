77d199c2d34c5e26b41eb5150868e78d4bcfbc88
Yucheng Cao <yuchengcao@tubitv.com>
2026-07-29 11:27:58 +0800

    [clojure] add clojure's embark config

--- doom/.doom.d/config-clojure.el
+++ doom/.doom.d/config-clojure.el
@@ -31,3 +31,51 @@
         :localleader
         :n "k" #'custom-eval-user-go)
   )
+
+;; NOTE: https://emacsredux.com/blog/2026/07/25/cider-and-projectile-meet-embark/
+(defun my-cider-embark-doc (sym)
+  (interactive "sClojure symbol: ")
+  (cider-doc-lookup sym))
+
+(defun my-cider-embark-find-def (sym)
+  (interactive "sClojure symbol: ")
+  (cider-find-var nil sym))
+
+(defun my-cider-embark-fn-refs (sym)
+  (interactive "sClojure symbol: ")
+  (cider-xref-fn-refs nil sym))
+
+(defun my-cider-embark-inspect (sym)
+  (interactive "sClojure symbol: ")
+  (cider-inspect-expr sym (cider-current-ns)))
+
+(defun my-cider-embark-clojuredocs (sym)
+  (interactive "sClojure symbol: ")
+  (cider-clojuredocs-lookup sym))
+
+(defun my-cider-embark-apropos (sym)
+  (interactive "sClojure symbol: ")
+  (cider-apropos sym))
+
+(defvar my-cider-embark-symbol-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "d" #'my-cider-embark-doc)
+    (define-key map "." #'my-cider-embark-find-def)
+    (define-key map "r" #'my-cider-embark-fn-refs)
+    (define-key map "i" #'my-cider-embark-inspect)
+    (define-key map "c" #'my-cider-embark-clojuredocs)
+    (define-key map "a" #'my-cider-embark-apropos)
+    map))
+
+(defun my-cider-embark-target ()
+  (when (derived-mode-p 'clojure-mode 'clojurescript-mode 'clojurec-mode
+                        'clojure-ts-mode 'cider-repl-mode)
+    (when-let* ((bounds (bounds-of-thing-at-point 'symbol))
+                (sym (cider-symbol-at-point)))
+      (unless (string-empty-p sym)
+        `(cider-clojure-symbol ,sym . ,bounds)))))
+
+(after! embark
+  (add-to-list 'embark-target-finders #'my-cider-embark-target)
+  (add-to-list 'embark-keymap-alist '(cider-clojure-symbol my-cider-embark-symbol-map))
+  (add-to-list 'embark-keymap-alist '(cider my-cider-embark-symbol-map)))
